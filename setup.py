2f72287c977ab1d449cdfa6c238c1e7a8e30f71d
Ana-Karem-Garcia <142260985+Ana-Karem-Garcia@users.noreply.github.com>
2023-11-22 15:18:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='Math_Travel',
       version='0.1',
       description='A Python package for something',
-      url='http://github.com/rvf0068/minimal',
-      author='Yo Mero',
-      author_email='yomero@gmail.com',
+      url='https://github.com/Ana-Karem-Garcia/Math_Travel',
+      author='Karem Garcia, Ivan Delgado, Claudia Salas, Tamara Popoca', 
+      author_email='ga382900@uaeh.edu.mx , de375363@uaeh.edu.mx , sa450250@uaeh.edu.mx , po357607@uaeh.edu.mx',
       license='MIT',
-      packages=['minimal'],
+      packages=['Math_Travel'],
       zip_safe=False)
